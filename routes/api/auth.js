8d2da1fce6594adc8fcf68b5b50f2bc16115e57c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-27 00:17:49 +0300

    add router login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,9 +3,12 @@ const router = express.Router();
 
 const { schemas } = require('../../models/user');
 const { validateBody } = require('../../decorators');
-// const { isValidId } = require('../../middlewares');
+
 const UserController = require('../../controllers/auth');
 // or /signup
 router.post('/register', validateBody(schemas.registerSchema), UserController.register);
+
+//  or signin
+router.post('/login', validateBody(schemas.loginSchema), UserController.login);
 
 module.exports = router;
