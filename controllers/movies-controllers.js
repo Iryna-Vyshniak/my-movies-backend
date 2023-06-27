39681cd78e2e624fecdafc73d177f627d6a09e81
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-27 22:36:11 +0300

    add populate

--- controllers/movies-controllers.js
+++ controllers/movies-controllers.js
@@ -7,11 +7,28 @@ const { HttpError } = require('../helpers');
 const { ctrlWrapper } = require('../decorators');
 
 // add movie`s owner and get movies who added them
+// add populate() - інструмент пошуку для поширення запиту - в колекції знаходить поле owner і замінює його розширеними даними
 const getAllMovies = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const result = await Movie.find({ owner }, '-createdAt -updatedAt');
+  const result = await Movie.find({ owner }, '-createdAt -updatedAt').populate(
+    'owner',
+    'name email'
+  );
   res.json(result);
 };
+
+/*  GET api/movies
+
+populate('owner') =>
+"owner": {
+            "_id": "649b26b1566b2b32f187865b",
+            "name": "Alex",
+            "email": "alex@gmail.com",
+            "password": "$2a$10$BvzBjdg9hh1yP1nf1BP4/OpquwG7Yff3hmQinfl.WWKEJXSBAjaNG",
+            "token": "",
+            "createdAt": "2023-06-27T18:13:05.485Z",
+            "updatedAt": "2023-06-27T18:13:05.485Z"
+        } */
 
 const getMovieById = async (req, res, next) => {
   const { id } = req.params;
