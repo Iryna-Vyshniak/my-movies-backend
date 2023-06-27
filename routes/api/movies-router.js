371f5865789d111884e26543659c8f41c714393c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-27 21:46:44 +0300

    add authentication middlewares - check login user - bearer and token, protect every request

--- routes/api/movies-router.js
+++ routes/api/movies-router.js
@@ -3,18 +3,21 @@ const express = require('express');
 const moviesController = require('../../controllers/movies-controllers');
 const { schemas } = require('../../models/movie');
 const { validateBody } = require('../../decorators');
-const { isValidId } = require('../../middlewares');
+const { isValidId, authenticate } = require('../../middlewares');
 
 const router = express.Router();
 
-router.get('/', moviesController.getAllMovies);
+// add authentication middlewares - check login user - bearer and token, protect every request
 
-router.get('/:id', isValidId, moviesController.getMovieById);
+router.get('/', authenticate, moviesController.getAllMovies);
 
-router.post('/', validateBody(schemas.movieAddSchema), moviesController.addMovie);
+router.get('/:id', authenticate, isValidId, moviesController.getMovieById);
+
+router.post('/', authenticate, validateBody(schemas.movieAddSchema), moviesController.addMovie);
 
 router.put(
   '/:id',
+  authenticate,
   isValidId,
   validateBody(schemas.movieAddSchema),
   moviesController.updateMovieById
@@ -22,11 +25,12 @@ router.put(
 
 router.patch(
   '/:id/favorite',
+  authenticate,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   moviesController.updateStatusMovie
 );
 
-router.delete('/:id', isValidId, moviesController.deleteMovieById);
+router.delete('/:id', authenticate, isValidId, moviesController.deleteMovieById);
 
 module.exports = router;
