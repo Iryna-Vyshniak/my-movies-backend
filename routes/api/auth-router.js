958dd18e07a8cd1d48c523a3c41b3a42028cac50
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-27 23:16:20 +0300

    add router GET auth current user

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -5,10 +5,13 @@ const { schemas } = require('../../models/user');
 const { validateBody } = require('../../decorators');
 
 const AuthController = require('../../controllers/auth-controllers');
+const { authenticate } = require('../../middlewares');
 // or /signup
 router.post('/register', validateBody(schemas.registerSchema), AuthController.register);
 
 //  or signin
 router.post('/login', validateBody(schemas.loginSchema), AuthController.login);
+
+router.get('/current', authenticate, AuthController.getCurrent);
 
 module.exports = router;
