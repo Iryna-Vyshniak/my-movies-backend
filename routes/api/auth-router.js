10d35f5e13ccbd229d1feebf89e3b0af9bd6106a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-04 16:52:28 +0300

    add routers to verify email

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -9,6 +9,11 @@ const { authenticate } = require('../../middlewares');
 // or /signup
 router.post('/register', validateBody(schemas.registerSchema), AuthController.register);
 
+// verify
+router.get('/verify/:verificationCode', AuthController.verify);
+// verify resend if didn`t send email
+router.post('/verify', validateBody(schemas.userEmailSchema), AuthController.resendVerify);
+
 //  or signin
 router.post('/login', validateBody(schemas.loginSchema), AuthController.login);
 
