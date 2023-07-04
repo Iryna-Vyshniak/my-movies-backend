930e7b26cbe31bf41575d7ad70c9147a3e44e750
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-04 15:55:13 +0300

    update

--- app.js
+++ app.js
@@ -2,34 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 // const ElasticEmail = require('@elasticemail/elasticemail-client');
-const nodemailer = require('nodemailer');
 require('dotenv').config();
-
-const { UKR_NET_EMAIL, UKR_NET_PASSWORD } = process.env;
-
-const nodemailerConfig = {
-  host: 'smtp.ukr.net',
-  port: 465, // 25, 465, 2525
-  secure: true,
-  auth: {
-    user: UKR_NET_EMAIL,
-    pass: UKR_NET_PASSWORD,
-  },
-};
-
-const transport = nodemailer.createTransport(nodemailerConfig);
-
-const email = {
-  from: UKR_NET_EMAIL,
-  to: 'heknirepsu@gufum.com',
-  subject: 'Verify email',
-  html: '<p>Verify email</p>',
-};
-
-transport
-  .sendMail(email)
-  .then(() => console.log('Verify email sent'))
-  .catch((err) => console.log(err.message));
 
 /* 
 const { ELASTICEMAIL_API_KEY } = process.env;
